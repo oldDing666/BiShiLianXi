1c33e474b3567dfefd9562272c490aeaa051c265
oldding666 <1101623201@qq.com>
2021-09-15 16:50:21 +0800

    T_14

--- src/JZOffer_v1/T_14_2.java
+++ src/JZOffer_v1/T_14_2.java
@@ -3,9 +3,9 @@ package JZOffer_v1;
 /**
  * @author: afuya
  * @program: BiShiLianXi
- * @date: 2021/9/15 4:19 下午
+ * @date: 2021/9/15 4:46 下午
  */
-public class T_14_1 {
+public class T_14_2 {
     public int cuttingRope(int n) {
         if (n == 2)
             return 1;
@@ -13,22 +13,27 @@ public class T_14_1 {
             return 2;
         int n1 = n % 3;
         int n2 = n / 3;
-        int sum = 1;
+        long sum = 1;
         if (n1 == 0) {
             for (int i = 0; i < n2; i++) {
                 sum *= 3;
+                sum %= 1e9 + 7;
             }
         } else if (n1 == 2) {
             for (int i = 0; i < n2; i++) {
                 sum *= 3;
+                sum %= 1e9 + 7;
             }
             sum *= 2;
+            sum %= 1e9 + 7;
         } else if (n1 == 1) {
             for (int i = 0; i < n2 - 1; i++) {
                 sum *= 3;
+                sum %= 1e9 + 7;
             }
             sum *= 4;
+            sum %= 1e9 + 7;
         }
-        return sum;
+        return (int) sum;
     }
 }
